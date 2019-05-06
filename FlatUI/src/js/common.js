f6dc42d97deaffc9826eabf9c536a2900860f469
Евгений <evgenprogrammer@gmail.com>
2019-05-06 09:52:05 +0300

    rebase project

--- FlatUI/src/js/common.js
+++
@@ -1,7 +0,0 @@
-
-import '../components/slider/slider'
-import '../components/percentages/percentages'
-import '../components/buttons/button'
-import '../components/stages/stage'
-import '../components/accordeon/accordeon'
-
